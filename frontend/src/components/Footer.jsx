98db0ad2720eeebe90c49ad877c7adc25526aa8f
Ravulaaravind <130533726+Ravulaaravind@users.noreply.github.com>
2025-06-20 10:21:59 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
                   rel="noopener noreferrer"
                   className="text-gray-300 hover:text-green-400 transition-colors text-xs md:text-base"
                 >
-                  4-7-62/2, Shivaji nagar, Attapur, Hyderabad City, Telangana state 500048.
+                    5-4/3
                 </a>
               </li>
               <li className="flex items-center justify-center space-x-2 md:space-x-3">
@@ -71,10 +71,10 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 <a 
-                  href="tel:+919701555435" 
+                  href="tel:+ +91 7207711919
                   className="text-gray-300 hover:text-green-400 transition-colors text-xs md:text-base"
                 >
-                  +91 9701555435
+                  +91  +91 7207711919
                 </a>
               </li>
               <li className="flex items-center justify-center space-x-2 md:space-x-3">
@@ -85,7 +85,7 @@ const Footer = () => {
                   href="mailto:support@vin2grow.com" 
                   className="text-gray-300 hover:text-green-400 transition-colors text-xs md:text-base"
                 >
-                  support@vin2grow.com
+                   vintogrow2@gmail.com
                 </a>
               </li>
             </ul>
